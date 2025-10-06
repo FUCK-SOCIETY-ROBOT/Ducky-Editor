5e5aceff863673092a7d19cfef92428a7285deae
FUCK-SOCIETY-ROBOT <nonecoderprogram@gmail.com>
2025-10-06 14:54:57 +0330

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    icon: path.join(__dirname, 'icon.ico'),
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  win.loadFile('editor.html');
+}
+
+app.whenReady().then(createWindow);
