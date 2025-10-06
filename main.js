09245d04c290b8e5649450e45735d1a98fe67e5b
FUCK-SOCIETY-ROBOT <nonecoderprogram@gmail.com>
2025-10-06 15:16:02 +0330

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
-    icon: path.join(__dirname, 'icon.ico'),
+    icon: path.join(__dirname, '1000299315.ico'),
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
